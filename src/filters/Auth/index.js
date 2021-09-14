afe76cdf6ded0d7dceb1d2bf32169bfa3cf89fa5
estefaneluz <estefanecortes@gmail.com>
2021-09-14 19:36:47 -0300

    filtro de autenticação criado

--- src/filters/Auth/index.js
+++ src/filters/Auth/index.js
@@ -1,8 +1,28 @@
 const jwt = require('jsonwebtoken');
+const { default: knex } = require('knex');
 const passwordHash = require('../../passwordHash');
 
 const auth = async (req, res, next) => {
+    const { authorization } = req.headers;
 
+    if(!authorization) return res.status(401).json("Não autorizado.")
+
+    try {
+        const token = authorization.replace('Bearer ', '').trim();
+
+        const { id } = jwt.verify(token, passwordHash);
+
+        const exits = await knex('usuarios').where({id}).first();
+        
+        if(!exits) return res.status(404).json("Usuário não encontrado.");
+        
+        const { password, ...user } = exits;
+        req.users = user;
+
+        next();
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
 }
 
 module.exports = auth;
